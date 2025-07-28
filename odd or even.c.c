6d188e075d153227eb4146461205f58f86c6bf7d
Junaid sande <junaidsande@gmail.com>
2025-07-28 19:06:05 +0530

    Add files via upload

---
+++ odd or even.c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() 
+	{
+	int a=10;
+		if (a%2==0)
+		{
+			printf("even");
+		}
+		else
+		{
+			printf("odd");
+		}
+}
---
+++ pallindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	
+    int a=121;
+	int b=121;
+    
+	if (a==b)
+	{
+		printf("number is pallindrome");
+	}
+	else
+	{
+		printf("number is not pallindrome");
+	}
+}
